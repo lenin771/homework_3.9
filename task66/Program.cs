0fe32a42c7854a930b57b9886c91e3a26b0ced2a
Your Name <you@example.com>
2022-09-29 18:54:08 +0300

    add task66

---
+++ task66/Program.cs
@@ -0,0 +1,31 @@
+﻿// Задача 66: Задайте значения M и N. Напишите программу, 
+// которая найдёт сумму натуральных элементов в промежутке от M до N.
+
+int sum = 0;
+ShowNumsBetween(2, 8);
+Console.Write($"\n Сумма элементов в диапазоне {sum} ");
+Console.WriteLine();
+
+void ShowNumsBetween(int min, int max)
+{
+    sum = min;
+    if (min == max)
+    {
+        Console.Write(min);
+        return;
+    }
+    if (min > max)
+    {
+        Console.Write($"{min} ");
+        ShowNumsBetween(min - 1, max);
+    }
+    else
+    {
+        Console.Write($"{max} ");
+        ShowNumsBetween(min, max - 1);
+    }
+    sum = sum + max;
+    
+}
+
+
